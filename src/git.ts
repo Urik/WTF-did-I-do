aa8a79853e06518df912a1703b011f72416e5903
Uri Berman <uribk@unito.io>
2022-03-27 02:24:55 -0400

    added error handling

--- src/git.ts
+++ src/git.ts
@@ -2,17 +2,22 @@ import simpleGit from 'simple-git';
 import { CommitLog } from './types/CommitLog';
 
 export async function getCommits(fromDate: Date, toDate: Date, author: string, path: string): Promise<CommitLog[]> {
-  const logs = await simpleGit(path).log([
-    '--after', fromDate.toISOString(),
-    '--before', toDate.toISOString(),
-    '--author', author,
-  ]);
+  try {
+    const logs = await simpleGit(path).log([
+      '--after', fromDate.toISOString(),
+      '--before', toDate.toISOString(),
+      '--author', author,
+    ]);
 
-  return logs.all.map(log => ({
-    body: log.body,
-    date: log.date,
-    message: log.message,
-    repository: path,
-    author: log.author_email
-  }));
+    return logs.all.map(log => ({
+      body: log.body,
+      date: log.date,
+      message: log.message,
+      repository: path,
+      author: log.author_email
+    }));
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 }
